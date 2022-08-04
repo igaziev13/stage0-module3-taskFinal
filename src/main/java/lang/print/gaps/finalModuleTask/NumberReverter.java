10ff3d9621fadff0506a4b9b456a064578152939
IGAZIEV <islam.gaziev@gmail.com>
2022-08-04 12:59:14 +0500

    Module #3. FINAL MODULE TASK. Attempt #1.

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int num1 = number / 100;
+        int num2 = (number % 100) / 10;
+        int num3 = (number % 10) * 100;
+
+        System.out.println(num1 + num2 * 10 + num3);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -3,5 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class NumbersPrinter {
     public void printIsPositive(int number) {
 
+        System.out.println(number > 0);
+
     }
 }
